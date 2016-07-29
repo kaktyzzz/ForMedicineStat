b62ea2a2f9b337f103f20d077f12bf0b00ec19b1
Maxim <ax1m1@rambler.com>
2016-07-29 19:39:47 +0300

    [add] some code

--- classification.py
+++ classification.py
@@ -1,1 +1,25 @@
-https://habrahabr.ru/post/202090/
+#https://habrahabr.ru/post/202090/
+
+from pandas import read_csv, DataFrame, Series
+from sklearn import cross_validation, svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve, auc
+import pylab as pl
+
+
+data = read_csv('pacient.csv', sep=';', header=None)
+print data
+
+target = data.Survived
+train = data.drop(['Survived'], axis=1) #из исходных данных убираем Id пассажира и флаг спасся он или нет
+kfold = 5 #количество подвыборок для валидации
+itog_val = {} #список для записи результатов кросс валидации разных алгоритмов
+
+ROCtrainTRN, ROCtestTRN, ROCtrainTRG, ROCtestTRG = cross_validation.train_test_split(train, target, test_size=0.25)
+
+model_rfc = RandomForestClassifier(n_estimators = 70) #в параметре передаем кол-во деревьев
+model_knc = KNeighborsClassifier(n_neighbors = 18) #в параметре передаем кол-во соседей
+model_lr = LogisticRegression(penalty='l1', tol=0.01)
+model_svc = svm.SVC() #по умолчанию kernek='rbf'
