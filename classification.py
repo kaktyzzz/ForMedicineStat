5fd0b79a4905ab69053370a0d3c37b5903ce8ddc
Maxim <ax1m1@rambler.com>
2016-08-01 19:13:51 +0300

    [add] ROC analize

--- classification.py
+++ classification.py
@@ -8,13 +8,65 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
+from scipy import interp
+import numpy as np
 import pylab as pl
+
+
+def ROCanalize(classificator_name, test, prob):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    n_classes = test.shape[1]
+    pl.figure()
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(test[:, i], prob[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        pl.plot(fpr[i], tpr[i], label='class %d (area = %0.2f)' % (i, roc_auc[i]))
+
+    fpr["micro"], tpr["micro"], _ = roc_curve(test.ravel(), prob.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    pl.plot(fpr["micro"], tpr["micro"],
+            label='micro-average ROC curve (area = {0:0.2f})'
+                  ''.format(roc_auc["micro"]),
+            linewidth=2)
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    pl.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             linewidth=2)
+
+    pl.plot([0, 1], [0, 1], 'k--')
+    pl.xlim([0.0, 1.0])
+    pl.ylim([0.0, 1.0])
+    pl.xlabel('False Positive Rate')
+    pl.ylabel('True Positive Rate')
+    pl.title('ROC ' + classificator_name)
+    pl.legend(loc=0, fontsize='small')
+    pl.show()
 
 
 data = read_csv('pacient.csv', sep=';', header=None)
 
 target = data[2]
-train = data.drop([0, 1, 2], axis=1) #из исходных данных убираем Id пассажира и флаг спасся он или нет
+train = data.drop([0, 1, 2], axis=1) #из исходных данных убираем
 
 kfold = 5 #количество подвыборок для валидации
 itog_val = {} #список для записи результатов кросс валидации разных алгоритмов
@@ -35,59 +87,27 @@ scores = cross_validation.cross_val_score(model_svc, train, target, cv = kfold)
 itog_val['SVC'] = scores.mean()
 
 print itog_val
-# DataFrame.from_dict(data = itog_val, orient='index').plot(kind='bar', legend=False)
 
-# pl.clf()
-# pl.figure(figsize=(8, 6))
+ROCtrainTRN, ROCtestTRN, ROCtrainTRG, ROCtestTRG = cross_validation.train_test_split(train, target, test_size=0.5)
+ROCtestTRG = label_binarize(ROCtestTRG, classes=[1, 2, 3, 4])
 
-
-ROCtrainTRN, ROCtestTRN, ROCtrainTRG, ROCtestTRG = cross_validation.train_test_split(train, target, test_size=0.25)
-# ROCtestTRG = label_binarize(ROCtestTRG, classes=[1, 2, 3, 4])
-# n_classes = ROCtestTRG.shape[1]
-n_classes = 4
 # #SVC
-# model_svc.probability = True
-# probas = model_svc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
+model_svc.probability = True
+#probas = model_svc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
 # fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1], pos_label=1)
 # roc_auc  = auc(fpr, tpr)
 # pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('SVC', roc_auc))
+
 #RandomForestClassifier
 probas = model_rfc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
-# print probas[:, 1]
-# fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1], pos_label=1)
-# roc_auc  = auc(fpr, tpr)
-# Compute ROC curve and ROC area for each class
+ROCanalize('RandomForestClassifier', ROCtestTRG, probas)
 
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-# print ROCtestTRG[:, 1]
-# print probas[:, 1]
-for i in range(4):
-    fpr[i], tpr[i], _ = roc_curve(ROCtestTRG, probas[:, i], pos_label=2)
-    roc_auc[i] = auc(fpr[i], tpr[i])
-    pl.plot(fpr[i], tpr[i], label='%s ROC class %d (area = %0.2f)' % ('RandomForest', i, roc_auc[i]))
-# Compute micro-average ROC curve and ROC area
-# fpr["micro"], tpr["micro"], _ = roc_curve(ROCtestTRG.ravel(), probas.ravel())
-# roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-# pl.plot(fpr["micro"], tpr["micro"],
-#          label='micro-average ROC curve (area = {0:0.2f})'
-#                ''.format(roc_auc["micro"]),
-#          linewidth=2)
-# # #KNeighborsClassifier
-# probas = model_knc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
-# fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1], pos_label=1)
-# roc_auc  = auc(fpr, tpr)
-# pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('KNeighborsClassifier',roc_auc))
-# #LogisticRegression
-# probas = model_lr.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
-# fpr, tpr, thresholds = roc_curve(ROCtestTRG, probas[:, 1], pos_label=1)
-# roc_auc  = auc(fpr, tpr)
-# pl.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % ('LogisticRegression',roc_auc))
-pl.plot([0, 1], [0, 1], 'k--')
-pl.xlim([0.0, 1.0])
-pl.ylim([0.0, 1.0])
-pl.xlabel('False Positive Rate')
-pl.ylabel('True Positive Rate')
-pl.legend(loc=0, fontsize='small')
-pl.show()
+#KNeighborsClassifier
+probas = model_knc.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
+ROCanalize('KNeighborsClassifier', ROCtestTRG, probas)
+
+#LogisticRegression
+probas = model_lr.fit(ROCtrainTRN, ROCtrainTRG).predict_proba(ROCtestTRN)
+ROCanalize('LogisticRegression', ROCtestTRG, probas)
+
+
